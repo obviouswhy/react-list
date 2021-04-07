3db0f8999914eb91203d62ab7fdf5f34d86fd3d9
obviouswhy <sergilockerz@gmail.com>
2021-04-07 17:56:35 +0900

    cleaned create-react

---
+++ src/components/LandingPage/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../LandingPage/LandingPage.css'
+
+const LandingPage = () => {
+  return (
+    <div className='landing-wrapper'>
+    </div>
+  )
+}
+
+export default LandingPage
---
+++ src/components/NavBar/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../NavBar/NavBar.css'
+
+const NavBar = () => {
+  return (
+    <div className='landing-wrapper'>
+    </div>
+  )
+}
+
+export default NavBar
