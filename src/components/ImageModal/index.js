08476eefb7cfbeb6908843e86d01ccc043f01c48
obviouswhy <sergilockerz@gmail.com>
2021-04-08 14:58:24 +0900

    added framer-motion + small changes

--- src/components/ImageModal/index.js
+++ src/components/ImageModal/index.js
@@ -6,7 +6,7 @@ const ImageModal = ({ selectedImg, setSelectedImg }) => {
   return (
     <div className='modal-wrapper'>
       <div onClick={() => setSelectedImg(null)} className='backdrop' >
-        <img src={selectedImg.urls.regular} alt={`enlarged-${selectedImg.description}`} />
+        <img src={selectedImg.urls.regular} alt={`enlarged-${selectedImg.description}`} onClick={e => e.stopPropagation()} />
       </div>
     </div>
   )
